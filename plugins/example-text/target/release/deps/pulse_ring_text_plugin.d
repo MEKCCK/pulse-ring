96e210e704f6d9df54d23ae4b225d2fe64af7121
MEKCCK <MEKCCK@users.noreply.github.com>
2026-08-11 19:44:29 +0800

    忽略插件构建产物（.so/target）

--- plugins/example-text/target/release/deps/pulse_ring_text_plugin.d
+++
@@ -1,5 +0,0 @@
-/home/elite/Projects/pulse-ring/plugins/example-text/target/release/deps/pulse_ring_text_plugin.d: src/lib.rs
-
-/home/elite/Projects/pulse-ring/plugins/example-text/target/release/deps/libpulse_ring_text_plugin.so: src/lib.rs
-
-src/lib.rs:
--- plugins/example-text/target/release/libpulse_ring_text_plugin.d
+++
@@ -1,1 +0,0 @@
-/home/elite/Projects/pulse-ring/plugins/example-text/target/release/libpulse_ring_text_plugin.so: /home/elite/Projects/pulse-ring/plugins/example-text/src/lib.rs
